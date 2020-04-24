b0b5b4b39f2d4e55199de732d06b8cd7ed83c7f8
Christopher Townsend <christopher.townsend@apexa.ca>
2020-04-24 10:54:28 -0400

    start application minimized and change display mode.

--- UsbEvent/Program.cs
+++ UsbEvent/Program.cs
@@ -13,28 +13,30 @@ namespace UsbEvent
         static readonly Guid GUID_DEVCLASS_USB = new Guid("{c166523c-fe0c-4a94-a586-f1a80cfbbf3e}");
         static readonly string NAME = "Microphone (Blue Snowball)";
 
+        static void RestartApplication(string processname)
+        {
+            var process = Process.GetProcessesByName(processname)[0];
+
+            if (process != null)
+            {
+                process.Kill();
+
+                ProcessStartInfo startInfo = new ProcessStartInfo(process.MainModule.FileName);
+
+                startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                startInfo.UseShellExecute = true;
+
+                Process.Start(startInfo);
+            }
+        }
+
         static void Main(string[] args)
         {
             try
             {
-                var query = new WqlEventQuery();
-                query.EventClassName = "__InstanceOperationEvent";
-                query.WithinInterval = new TimeSpan(0, 0, 2);
-                query.Condition = @"TargetInstance ISA 'Win32_PnPEntity'";
-
-                ManagementEventWatcher watcher = new ManagementEventWatcher();
-                //WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2");
-                watcher.EventArrived += new EventArrivedEventHandler(watcher_EventArrived);
-                watcher.Query = query;
-                watcher.Start();
-
-                while (true)
-                {
-                    watcher.WaitForNextEvent();
-                }
-                
-
-            }catch(Exception ex)
+                StartMonitoring();
+            }
+            catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 Console.ReadKey();
@@ -42,6 +44,53 @@ namespace UsbEvent
        
         }
 
+        private static void StartMonitoring()
+        {
+            var query = new WqlEventQuery();
+            query.EventClassName = "__InstanceOperationEvent";
+            query.WithinInterval = new TimeSpan(0, 0, 2);
+            query.Condition = @"TargetInstance ISA 'Win32_PnPEntity'";
+
+            ManagementEventWatcher watcher = new ManagementEventWatcher();
+            watcher.EventArrived += new EventArrivedEventHandler(watcher_EventArrived);
+            watcher.Query = query;
+            watcher.Start();
+
+            while (true)
+            {
+                watcher.WaitForNextEvent();
+            }
+
+        }
+
+        private static void SetDisplayMode(DisplayMode mode)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = "DisplaySwitch.exe";
+            switch (mode)
+            {
+                case DisplayMode.External:
+                    proc.StartInfo.Arguments = "/external";
+                    break;
+                case DisplayMode.Internal:
+                    proc.StartInfo.Arguments = "/internal";
+                    break;
+                case DisplayMode.Extend:
+                    proc.StartInfo.Arguments = "/extend";
+                    break;
+                case DisplayMode.Duplicate:
+                    proc.StartInfo.Arguments = "/clone";
+                    break;
+            }
+            proc.Start();
+        }
+        enum DisplayMode
+        {
+            Internal,
+            External,
+            Extend,
+            Duplicate
+        }
         private static void watcher_EventArrived(object sender, EventArrivedEventArgs e)
         {
             ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
@@ -50,22 +99,30 @@ namespace UsbEvent
             Console.WriteLine($"{name} - {(string)instance["Name"]} ({(string)instance["ClassGuid"]})");
 
             if (new Guid((string)instance["ClassGuid"]) == GUID_DEVCLASS_USB 
-                && ((string)instance["Name"]).Replace(" " , String.Empty).Equals(NAME.Replace(" ", String.Empty), StringComparison.InvariantCultureIgnoreCase)
-                && name == "__InstanceCreationEvent")
+                && ((string)instance["Name"]).Replace(" " , String.Empty).Equals(NAME.Replace(" ", String.Empty), StringComparison.InvariantCultureIgnoreCase) )
             {
-                Console.WriteLine("restarting reaper");
-
-                var process = Process.GetProcessesByName("REAPER")[0];
-
-                if (process != null)
+                switch(name)
                 {
-                    process.Kill();
-     
-                    ProcessStartInfo startInfo = new ProcessStartInfo(process.MainModule.FileName);
-                    startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                    case "__InstanceCreationEvent":
+                        {
+                            Console.WriteLine("Restarting Reaper");
+                            RestartApplication("REAPER");
 
-                    Process.Start(startInfo);
+                            Console.WriteLine("SetDisplayMode(Extend)");
+                            SetDisplayMode(DisplayMode.Extend);
+
+                            break;
+                        }
+                    case "__InstanceDeletionEvent":
+                        {
+                            Console.WriteLine("SetDisplayMode(Duplicate)");
+                            SetDisplayMode(DisplayMode.Duplicate);
+
+                            break;
+                        }
                 }
+
+                
                 
             }
         }
